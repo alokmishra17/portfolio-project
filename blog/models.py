027e72d5c23162d504e1f4833e59231d350348ba
Alok Mishra <ak17@localhost.localdomain>
2020-06-28 06:31:50 +0530

    first commit upgrade to django3

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+#create a blog model
+class Blog(models.Model):
+	title = models.CharField(max_length=100)
+	pub_date=models.DateTimeField()
+	body=models.TextField()
+	image=models.ImageField(upload_to='images/')
+
+	def summary(self):
+		return self.body[:200]
+
+	def pub_date_formatted(self):
+		return self.pub_date.strftime('%b %e %Y') 
+
+	def __str__(self):
+		return self.title
+		
